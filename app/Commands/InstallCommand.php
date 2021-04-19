9c1859276c6a6c9bebadf5f8a1fc8e0d6fa163e0
Mior Muhammad Zaki <crynobone@gmail.com>
2021-04-19 10:56:15 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- app/Commands/InstallCommand.php
+++ app/Commands/InstallCommand.php
@@ -3,6 +3,7 @@
 namespace App\Commands;
 
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Support\Facades\File;
 use LaravelZero\Framework\Commands\Command;
 
 class InstallCommand extends Command
