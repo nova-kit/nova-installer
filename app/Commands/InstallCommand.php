a1f9500756c80046766af45490f59b92af6d25ed
Mior Muhammad Zaki <crynobone@gmail.com>
2022-04-07 05:15:34 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- app/Commands/InstallCommand.php
+++ app/Commands/InstallCommand.php
@@ -18,7 +18,8 @@ class InstallCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'install {--working-path= : Project working directory}
+    protected $signature = 'install {name}
+                                    {--working-path= : Project working directory}
                                     {--install-optional : Install all optional dependencies}
                                     {--with-sample-data : With sample data}';
 
@@ -36,9 +37,12 @@ class InstallCommand extends Command
      */
     public function handle()
     {
+        $projectName = $this->argument('name');
         $workingPath = $this->option('working-path') ?? getcwd();
         $php = $this->findPhpBinary();
         $composer = $this->findComposer($workingPath);
+
+        $this->configureDatabase($projectName, $workingPath);
 
         if ($this->option('install-optional') === true) {
             $this->requireHelperPackages($composer, $workingPath);
@@ -51,6 +55,81 @@ class InstallCommand extends Command
         }
 
         $this->runDatabaseMigrations($php, $composer, $workingPath);
+    }
+
+    /**
+     * Configure database.
+     */
+    protected function configureDatabase(string $projectName, string $workingPath): void
+    {
+        $this->task('Configure Database', function () use ($projectName, $workingPath) {
+            $database = $this->menu('Choose Database Driver', [
+                'mysql',
+                'sqlite'
+            ])->open();
+
+            $phpBinary = $this->findPhpBinary();
+
+            if ($database == 0) {
+                return $this->configureMySqlDatabase($phpBinary, $projectName, $workingPath);
+            } elseif ($database == 1) {
+                return $this->configureSqliteDatabase($phpBinary, $projectName, $workingPath);
+            }
+
+            return false;
+        });
+    }
+
+    /**
+     * Configure database.
+     */
+    protected function configureMySqlDatabase(string $phpBinary, string $projectName, string $workingPath): bool
+    {
+        $db['HOST'] = $this->ask('Database Host?', $defaults['HOST'] = '127.0.0.1');
+        $db['PORT'] = $this->ask('Database Port?', $defaults['PORT'] = '3306');
+        $db['USERNAME'] = $this->ask('Database Username?', $defaults['USERNAME'] = 'root');
+        $db['PASSWORD'] = $this->ask('Database Password?', $defaults['PASSWORD'] = null);
+        $db['DATABASE'] = $this->ask('Database Name?', $defaults['DATABASE'] = str_replace('-', '_', strtolower($projectName)));
+
+        Terminal::builder()
+            ->in(getcwd())
+            ->run(
+                "mysql --user={$db['USERNAME']} --password={$db['PASSWORD']} --host={$db['HOST']} --port={$db['PORT']} -e \"create database {$db['DATABASE']};\""
+            );
+
+        $commands = collect();
+
+        foreach (['HOST', 'PORT', 'USERNAME', 'PASSWORD', 'DATABASE'] as $type) {
+            if ($db[$type] !== $defaults[$type]) {
+                $commands->push(
+                    "{$phpBinary} artisan tinker --execute=\"file_put_contents('.env', str_replace(['DB_{$type}={$defaults[$type]}'], ['DB_{$type}={$db[$type]}'], file_get_contents('.env')));\""
+                );
+            }
+        }
+
+        foreach ($commands as $command) {
+            Terminal::builder()->in($workingPath)->run($command);
+        }
+
+        return true;
+    }
+
+    protected function configureSqliteDatabase(string $phpBinary, string $projectName, string $workingPath): bool
+    {
+        $defaultDatabase = str_replace('-', '_', strtolower($projectName));
+
+        touch("{$workingPath}/database/database.sqlite");
+
+        $commands = collect([
+            "{$phpBinary} artisan tinker --execute=\"file_put_contents('.env', str_replace(['DB_CONNECTION=mysql'], ['DB_CONNECTION=sqlite'], file_get_contents('.env')));\"",
+            "{$phpBinary} artisan tinker --execute=\"file_put_contents('.env', str_replace(['DB_DATABASE={$defaultDatabase}'], ['# DB_DATABASE={$defaultDatabase}'], file_get_contents('.env')));\"",
+        ]);
+
+        foreach ($commands as $command) {
+            Terminal::builder()->in($workingPath)->run($command);
+        }
+
+        return true;
     }
 
     /**
